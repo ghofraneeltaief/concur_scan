a99ba353d68b43c1485ba1423945b83213ee56cf
GhofraneHipto <Ghofrane@hipto.com>
2024-05-30 12:04:04 +0100

    first commit

--- src/views/utm_stats/component/charts/Periode.jsx
+++ src/views/utm_stats/component/charts/Periode.jsx
@@ -1,116 +1,65 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { BASE_URL, api_version } from '../../../authentication/config';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-class Periode extends React.Component {
-  constructor(props) {
-    super(props);
+const apiData = [
+    { fb_ad_status: "0", hour: "9", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "10", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "11", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "12", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "13", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "14", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "15", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "16", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "17", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "18", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "19", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "20", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "21", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "22", total_cover: "1080434" },
+    { fb_ad_status: "0", hour: "23", total_cover: "1080434" },
+];
 
-    this.state = {
-    
-      series: [
-        // George Washington
-        {
-          name: 'George Washington',
-          data: [
-            {
-              x: '1',
-              y: [
-                new Date(1789, 3, 30).getTime(),
-                new Date(1797, 2, 4).getTime()
-              ]
-            },
-          ]
-        },
-        // John Adams
-        {
-          name: 'John Adams',
-          data: [
-            {
-              x: '1',
-              y: [
-                new Date(1797, 2, 4).getTime(),
-                new Date(1801, 2, 4).getTime()
-              ]
-            },
-          ]
-        },
-      ],
-      options: {
-        chart: {
-          height: 350,
-          type: 'rangeBar'
-        },
-        plotOptions: {
-          bar: {
-            horizontal: true,
-            barHeight: '50%',
-            rangeBarGroupRows: true
-          }
-        },
-        colors: [
-          "#008FFB", "#00E396"
-        ],
-        fill: {
-          type: 'solid'
-        },
-        xaxis: {
-          type: 'datetime',
-          show: false
-        },
-        legend: {
-          position: 'right'
-        },
-        tooltip: {
-          custom: function(opts) {
-            const fromYear = new Date(opts.y1).getFullYear()
-            const toYear = new Date(opts.y2).getFullYear()
-        
-            const w = opts.ctx.w
-            let ylabel = w.globals.labels[opts.dataPointIndex]
-            let seriesName = w.config.series[opts.seriesIndex].name
-              ? w.config.series[opts.seriesIndex].name
-              : ''
-            const color = w.globals.colors[opts.seriesIndex]
-        
-            return (
-              '<div class="apexcharts-tooltip-rangebar">' +
-              '<div> <span class="series-name" style="color: ' +
-              color +
-              '">' +
-              (seriesName ? seriesName : '') +
-              '</span></div>' +
-              '<div> <span class="category">' +
-              ylabel +
-              ' </span> <span class="value start-value">' +
-              fromYear +
-              '</span> <span class="separator">-</span> <span class="value end-value">' +
-              toYear +
-              '</span></div>' +
-              '</div>'
-            )
-          }
-        }
-      },
-    
-    
-    };
-  }
+const generateFullHourRange = () => {
+    const hours = [];
+    for (let i = 7; i <= 23; i++) {
+        hours.push(i);
+    }
+    return hours;
+};
 
+const formatData = (data) => {
+    const fullHours = generateFullHourRange();
+    const formattedData = fullHours.map(hour => {
+        const found = data.find(item => parseInt(item.hour) === hour);
+        return {
+            hour,
+            status: found ? 'Active' : 'Inactive'
+        };
+    });
+    return formattedData;
+};
 
+const TimelineChart = () => {
+    const [chartData, setChartData] = useState([]);
 
-  render() {
+    useEffect(() => {
+        // You would fetch data here, for now we use the static data provided
+        const formattedData = formatData(apiData);
+        setChartData(formattedData);
+    }, []);
+
     return (
-      <div>
-        <div id="chart">
-          <ReactApexChart options={this.state.options} series={this.state.series} type="rangeBar" height={350} />
-        </div>
-        <div id="html-dist"></div>
-      </div>
+        <ResponsiveContainer width="100%" height={100}>
+            <LineChart data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="hour" domain={[7, 23]} ticks={[7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]} />
+                <YAxis type="category" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="status" stroke="#82ca9d" />
+            </LineChart>
+        </ResponsiveContainer>
     );
-  }
-}
+};
 
-export default Periode;
+export default TimelineChart;

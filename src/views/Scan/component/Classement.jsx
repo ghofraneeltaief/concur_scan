d3f5a5ff8721cf0aa8756e92522bab47894469a1
GhofraneHipto <Ghofrane@hipto.com>
2024-06-03 15:26:25 +0100

    first commit

--- src/views/Scan/component/Classement.jsx
+++ src/views/Scan/component/Classement.jsx
@@ -16,12 +16,12 @@ import Paper from '@mui/material/Paper';
 import { Visibility } from '@mui/icons-material';
 import axios from 'axios';
 
-function Classement({onSelectedPage}) {
+function Classement({ onSelectedPage }) {
   const [concurrentList, setConcurrentList] = useState([]);
   const [pageList, setPageList] = useState([]);
   const [selectedConcurrent, setSelectedConcurrent] = useState(null);
   const [selectedPage, setSelectedPage] = useState(null);
-  /* begin: get token */
+
   async function getToken() {
     const token = localStorage.getItem('token');
     if (token) {
@@ -30,7 +30,7 @@ function Classement({onSelectedPage}) {
       throw new Error('No token available');
     }
   }
-  /* END: get token */
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -64,6 +64,7 @@ function Classement({onSelectedPage}) {
     };
     fetchData();
   }, []);
+
   const handleConcurrentClick = (concurrent) => {
     if (selectedConcurrent && selectedConcurrent.competitor_id === concurrent.competitor_id) {
       setSelectedConcurrent(null);
@@ -72,10 +73,18 @@ function Classement({onSelectedPage}) {
     }
     setSelectedPage(null);
   };
+
   const handlePageClick = (page) => {
     setSelectedPage(page);
     onSelectedPage(page.page_id);
   };
+
+  useEffect(() => {
+    if (selectedPage === null) {
+      onSelectedPage(null);
+    }
+  }, [selectedPage, onSelectedPage]);
+
   const countPagesByCompetitor = () => {
     const pageCounts = {};
     pageList.forEach((page) => {
@@ -88,20 +97,18 @@ function Classement({onSelectedPage}) {
     });
     return pageCounts;
   };
+
   const pagesByCompetitor = countPagesByCompetitor();
   const filteredPageList = selectedConcurrent
     ? pageList.filter((page) => page.fk_competitor_id === selectedConcurrent.competitor_id)
     : pageList;
-  
+
   return (
     <Box sx={{ width: 1 }}>
       <Box display="grid" gridTemplateColumns="repeat(16, 1fr)" gap={2}>
         <Box gridColumn="span 6">
           <DashboardCard title="Classement concurrent" subtitle="par nombre d’ouverture" height="500px">
-            <TableContainer
-              component={Paper}
-              sx={{ boxShadow: 'none', overflowY: 'auto' }}
-            >
+            <TableContainer component={Paper} sx={{ boxShadow: 'none', overflowY: 'auto' }}>
               <Table stickyHeader>
                 <TableHead>
                   <TableRow sx={{ backgroundColor: '#F7F9FC' }}>
@@ -111,41 +118,38 @@ function Classement({onSelectedPage}) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-            {concurrentList.map((row, index) => (
-                <TableRow
-                  key={index}
-                  onClick={() => handleConcurrentClick(row)}
-                  sx={{
-                    cursor: 'pointer',
-                    backgroundColor: selectedConcurrent?.competitor_id === row.competitor_id ? '#E0F7FA' : 'inherit',
-                    '&:hover': { backgroundColor: '#F1F1F1' },
-                  }}
-                >
-                  <TableCell>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                      <IconButton sx={{ padding: '4px', marginRight: '8px' }}>
-                        <Visibility sx={{ fontSize: '16px', color: '#0095E8' }} />
-                      </IconButton>
-                      <Typography color={selectedConcurrent?.competitor_id === row.competitor_id ? '#0095E8' : 'inherit'}>
-                        {row.competitor_name}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                  <TableCell>{pagesByCompetitor[row.competitor_id] || 0}</TableCell>
-                  <TableCell>{row.couver}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
+                  {concurrentList.map((row, index) => (
+                    <TableRow
+                      key={index}
+                      onClick={() => handleConcurrentClick(row)}
+                      sx={{
+                        cursor: 'pointer',
+                        backgroundColor: selectedConcurrent?.competitor_id === row.competitor_id ? '#E0F7FA' : 'inherit',
+                        '&:hover': { backgroundColor: '#F1F1F1' },
+                      }}
+                    >
+                      <TableCell>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconButton sx={{ padding: '4px', marginRight: '8px' }}>
+                            <Visibility sx={{ fontSize: '16px', color: '#0095E8' }} />
+                          </IconButton>
+                          <Typography color={selectedConcurrent?.competitor_id === row.competitor_id ? '#0095E8' : 'inherit'}>
+                            {row.competitor_name}
+                          </Typography>
+                        </Box>
+                      </TableCell>
+                      <TableCell>{pagesByCompetitor[row.competitor_id] || 0}</TableCell>
+                      <TableCell>{row.couver}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
               </Table>
             </TableContainer>
           </DashboardCard>
         </Box>
         <Box gridColumn="span 6">
           <DashboardCard title="Classement page" subtitle="pour le concurrent sélectionné" height="500px">
-            <TableContainer
-              component={Paper}
-              sx={{ boxShadow: 'none', overflowY: 'auto' }}
-            >
+            <TableContainer component={Paper} sx={{ boxShadow: 'none', overflowY: 'auto' }}>
               <Table stickyHeader>
                 <TableHead>
                   <TableRow sx={{ backgroundColor: '#F7F9FC' }}>
@@ -155,31 +159,31 @@ function Classement({onSelectedPage}) {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-            {filteredPageList.map((row, index) => (
-                <TableRow
-                  key={index}
-                  onClick={() => handlePageClick(row)}
-                  sx={{
-                    cursor: 'pointer',
-                    backgroundColor: selectedPage?.page_id === row.page_id ? '#E0F7FA' : 'inherit',
-                    '&:hover': { backgroundColor: '#F1F1F1' },
-                  }}
-                >
-                  <TableCell>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                      <IconButton sx={{ padding: '4px', marginRight: '8px' }}>
-                        <Visibility sx={{ fontSize: '16px', color: '#0095E8' }} />
-                      </IconButton>
-                      <Typography color={selectedPage?.page_id === row.page_id ? '#0095E8' : 'inherit'}>
-                        {row.page_name}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                  <TableCell>{row.nbr_ads}</TableCell>
-                  <TableCell>{row.couver}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
+                  {filteredPageList.map((row, index) => (
+                    <TableRow
+                      key={index}
+                      onClick={() => handlePageClick(row)}
+                      sx={{
+                        cursor: 'pointer',
+                        backgroundColor: selectedPage?.page_id === row.page_id ? '#E0F7FA' : 'inherit',
+                        '&:hover': { backgroundColor: '#F1F1F1' },
+                      }}
+                    >
+                      <TableCell>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconButton sx={{ padding: '4px', marginRight: '8px' }}>
+                            <Visibility sx={{ fontSize: '16px', color: '#0095E8' }} />
+                          </IconButton>
+                          <Typography color={selectedPage?.page_id === row.page_id ? '#0095E8' : 'inherit'}>
+                            {row.page_name}
+                          </Typography>
+                        </Box>
+                      </TableCell>
+                      <TableCell>{row.nbr_ads}</TableCell>
+                      <TableCell>{row.couver}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
               </Table>
             </TableContainer>
           </DashboardCard>

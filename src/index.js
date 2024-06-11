7db7020650022ff960a54c810a9bf4723a8fd652
GhofraneHipto <Ghofrane@hipto.com>
2024-06-11 15:30:08 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+var n = "%c\n" +
+" _    _   _____  ____    _____   ____ \n" + 
+"| |  | | |_   _| |  _ \\ |_   _| / __ \\ \n" + 
+"| |__| |   | |   | |_) |  | |  | |  | |   \n" + 
+"|  __  |   | |   |  __/   | |  | |  | | \n" + 
+"| |  | |  _| |_  | |      | |  | |__| | \n" + 
+"|_|  |_| |_____| |_|      | |   \\____/ \n" + 
+"      Rejoignez-nous ! #hipto            \n";
 
+console.log(n, "color:blue; font-weight:bold; font-family:monospace;");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Suspense>
